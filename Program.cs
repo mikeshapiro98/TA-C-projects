bca8098d95a89bb99e4297616f5b08e15139a182
Mike Shapiro <michaelshapiro98@gmail.com>
2018-05-12 22:12:09 -0700

    test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "this is a test";
+            Console.WriteLine(test);
+            Console.ReadLine();
+           
+
+        }
+    }
+}
