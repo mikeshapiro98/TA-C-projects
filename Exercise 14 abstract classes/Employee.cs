2759f7c386fcb4a5596f201e33f03c8f70535f23
Mike Shapiro <michaelshapiro98@gmail.com>
2018-05-30 22:35:39 -0700

    Exercise 14 1st commit

---
+++ Exercise 14 abstract classes/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_14_abstract_classes
+{
+    public class Employee : Person
+    {
+
+    
+
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+    }
+}
+
+//1. Create an abstract class called Person with two properties: string firstName and string lastName.
+
+//2. Give it the method SayName().
+
+//3. Create another class called Employee and have it inherit from the Person class.
+
+//4. Implement the SayName() method inside of the Employee class.
+
+//5. Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object.
