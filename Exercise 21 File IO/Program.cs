57b12af1cd849bda7ed9252838e60689024c6c9e
Mike Shapiro <michaelshapiro98@gmail.com>
2018-06-06 23:17:17 -0700

    Exercise 21 1st commit

---
+++ Exercise 21 File IO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This porgram is designed to exemplify the use of File I/O");
+            Console.WriteLine("Please enter a number to have it logged and then printed back to you");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
