cb573ed86706ba81eb0b5b7f30a1844e5fa72589
Mike Shapiro <michaelshapiro98@gmail.com>
2018-06-08 19:45:26 -0700

    Exercise 23 1st commit.

---
+++ Exercise 23 Extras/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_23_Extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INTRODUCTION
+            Console.WriteLine("Hello again. In this program we will be going over some extra features on C# not previously covered. \nPress enter to get started.");
+            Console.ReadLine();
+            //END INTRO
+
+            //CONSTANT VARIABLE
+            const string conString = "You can't change me :P ";
+            const int conInt = 28;
+            Console.WriteLine("\"{0}\" and {1}", conString, conInt);
+            Console.WriteLine("The above are examples of constant variables and therfore cannot be changed. Press enter to continue.");
+            Console.ReadLine();
+            //END CONSTANT VARIABLES.
+
+            //"Var" keyword
+            var stringVar = "I was created using the var keyword";
+            var intVar = 101;
+            Console.WriteLine("\"{0}\" and {1} are variables created using the \"var\" keyword", stringVar, intVar);
+            Console.WriteLine("These ones are examples of variables defined by using only the var keyword. Press enter to continue.");
+            Console.ReadLine();
+            //END "Var" Keyword
+
+            //CHAIN CONSTRUCTORS
+            Console.WriteLine("The following superheros and their information was assembled using multiple chain constructors.");
+     
+            List<Superhero> heroes = new List<Superhero>
+            {
+               new Superhero ( "Superman", "Superhuman everything"),
+               new Superhero ( "Batman has no jurisdiction...(Kind of Gotham City)"),
+               new Superhero ( ),
+               new Superhero ( "Wonder Woman", "Washington D.C.", "Superhuman Strength & Lasso Skills")
+            };
+
+
+            foreach (Superhero hero in heroes)
+            {
+                Console.WriteLine("\nName: " + hero.name + " \nCity of Jurisdiction: " + hero.city + " \nSuperpower: " + hero.superpower);
+
+            }
+
+            Console.WriteLine("Press enter to conclude.");
+            Console.ReadLine();
+            //END CHAIN CONSTRUCTORS
+
+            //CONCLUSION
+            Console.WriteLine("Hope you had fun. Goodbye for now!");
+            Console.ReadLine();
+            //END CONCLUSION
+
+
+
+
+
+        }
+
+
+    }
+}
+
+
+
---
+++ Exercise 23 Extras/Superhero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_23_Extras
+{
+    public class Superhero
+    {
+
+
+        public string name { get; set; }
+        public string city { get; set; }
+        public string superpower { get; set; }
+
+
+
+
+        public Superhero(string Name, string Superpower) : this(Name, "Metropolis", Superpower)
+        {
+        }
+        public Superhero(string City) : this("Batman", City, "Tons of Money")
+        { 
+        }
+        public Superhero() : this("Spiderman", "New York City", "Spidey Senses") 
+        {
+           
+        }
+        public Superhero(string Name, string City, string Superpower )
+        {
+
+            name = Name;
+            city = City;
+            superpower = Superpower;
+
+            
+
+        }
+
+     
+        
+
+
+    }
+}
