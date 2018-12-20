908491f252ffeb267229f73ecf30a03439bf21b5
Mike Shapiro <michaelshapiro98@gmail.com>
2018-12-20 12:02:05 -0800

    Re-work for clarity.

--- Exercise 13 Inheritance/Program.cs
+++ Exercise 13 Inheritance/Program.cs
@@ -20,8 +20,6 @@ namespace Exercise_13_Inheritance
         }
     }
 }
-
-
 //1. Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
 
 //2. Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: "Name: [full name]".
