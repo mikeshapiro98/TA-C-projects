1ed642b3fa6aa52d1ae8b53e481fb176afe76f99
Mike Shapiro <michaelshapiro98@gmail.com>
2018-12-24 11:57:02 -0800

    Student sugesstion

--- Exercise 5 Arrays and Lists/Program.cs
+++ Exercise 5 Arrays and Lists/Program.cs
@@ -69,16 +69,11 @@ namespace Exercise_5_Arrays_and_Lists
                 }
             }
             //END INT ARRAY
-
-
             //TRANSITION
             Console.WriteLine("Those were Arrays. Now let's move on to a list. Press enter to continue.");
             Console.ReadLine();
             //END TRANSITION
-
-
             //LIST 
-
             bool valid3 = false;
             while (valid3 == false)
             {
@@ -89,25 +84,19 @@ namespace Exercise_5_Arrays_and_Lists
                 stringList.Add("I am index 4 in this list");
                 stringList.Add("I am index 5 in this list");
                 stringList.Add("I am index 6 in this list");
-
                 Console.WriteLine("One more time. Type in an index place between 0 and 6 to see what's in that index and press enter");
                 string testIndex3 = Console.ReadLine();
                 int userIndex3;
                 bool test2 = int.TryParse(testIndex3, out userIndex3);
-
                 if (test2 == false)
                 {
-                    
                     Console.WriteLine("There was an error. Please enter only whole numbers between 0 and 6");
                     //break;
                 }
-
                 else if (userIndex3 < 0 || userIndex3 > 6)
                 {
-                    
                     Console.WriteLine("There was an error. Please enter only whole numbers between 0 and 6");
                 }
-
                 else
                 {
                     Console.WriteLine("You entered index number " + userIndex3 + ". The informaton in that index is: \n" + stringList[userIndex3] + "\nPress enter to continue");
@@ -115,12 +104,10 @@ namespace Exercise_5_Arrays_and_Lists
                     valid3 = true;
                 }
             }
-
             //CONCLUSION
             Console.WriteLine("This concludes this program designed to exemplify arrays and list. Thank you for completing it. Press enter to \nclose this window.");
             Console.ReadLine(); 
             //END CONCLUSION
-
         }
     }
 }
